7fde8202ae86ee83aede919312a1b7c7d9d13038
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-08-03 15:42:29 +0900

    Create 5554.py

---
+++ baekjoon/5554.py
@@ -0,0 +1,10 @@
+# 심부름 가는 길
+
+hTos = int(input())
+sToPC = int(input())
+PCToc = int(input())
+cToh = int(input())
+
+total_time = hTos + sToPC + PCToc + cToh
+print(total_time // 60)
+print(total_time % 60)
