022b3ea1abf2c6e55c9378ac13f0a358178b5348
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2022-08-04 15:36:40 +0900

    Create 1264.py

---
+++ baekjoon/1264.py
@@ -0,0 +1,13 @@
+# 모음의 개수
+
+while True:
+    s = input()
+    if s == '#':
+        break
+    count = 0
+  
+    for ch in s:
+        if ch in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            count += 1
+
+    print(count)
